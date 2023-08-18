b2beee7c29665dae75447ff07fe1d121844c01e2
Cemus <24947582+Cemus@users.noreply.github.com>
2023-08-18 18:48:01 +0200

    Change api target

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import config from "./env";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: config.apiBaseUrl,
+        target: "https://tame-pear-dugong-cape.cyclic.cloud/",
         changeOrigin: true,
         secure: false,
         ws: true,
