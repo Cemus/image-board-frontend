aac70630e1040a1eb62bbd0178a76851d4a69932
Cemus <24947582+Cemus@users.noreply.github.com>
2023-10-10 12:24:52 +0200

    Fetching image from api

--- src/components/ThreadThumbnail.tsx
+++ src/components/ThreadThumbnail.tsx
@@ -15,14 +15,14 @@ export default function ThreadThumbnail({
   image,
 }: ThreadProps) {
   /*   const imageUrl = image.substring(7, image.length); */
-  console.log(`${config.apiBaseUrl}/api/${image}`);
+  console.log(`${config.apiBaseUrl}/api/images/${image}`);
   return (
     <div className="flex flex-col items-center gap-1 p-2">
       <Link className="flex items-center justify-center" to={`/thread/${id}`}>
         <img
           className=" cursor-pointer rounded-sm max-h-96"
           loading="lazy"
-          src={`${config.apiBaseUrl}/api/${image}`}
+          src={`${config.apiBaseUrl}/api/images/${image}`}
           alt={`thumbnail-${id}`}
         />
       </Link>
